5d734972adfb99f0ca14625b2a2b3281d5b9c055
VitorCostaF <vitor.costa.comp@gmail.com>
2024-01-30 08:54:16 -0300

    -Adjustment of hot decorations query

--- party-manager/src/main/java/br/com/ieoafestasedecoracoes/partymanager/repository/DecorationRepository.java
+++ party-manager/src/main/java/br/com/ieoafestasedecoracoes/partymanager/repository/DecorationRepository.java
@@ -13,7 +13,16 @@ public interface DecorationRepository extends JpaRepository<Decoration, Integer>
 	List<Decoration> findByCategoriesId(Integer categoryId);
 	List<Decoration> findByCompanyId(Integer compantyId);
 	
-	@Query("SELECT d FROM Decoration d JOIN d.rentDecorations r ORDER BY r.startRentDate")
+	@Query("""
+			SELECT d FROM Decoration d 
+			JOIN
+			(SELECT d.id as id, r.startRentDate as rendDate, COUNT(r.decorationId) cont FROM Decoration d 
+			  JOIN d.rentDecorations r 
+			  ON r.decorationId = d.id
+			  GROUP BY d.id, r.startRentDate) t
+			ON d.id = t.id
+			ORDER BY t.rendDate DESC, t.cont DESC
+			""")
 	List<Decoration> findHotDecorations();
 
 }
